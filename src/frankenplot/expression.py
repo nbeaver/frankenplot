66d3bb602411a3003a9fe6b2e257cebe7c29c850
Jason Petsod <jason@petsod.org>
2010-05-04 13:12:41 -0500

    implement less braindead expression support
    - Expression objects!

--- src/frankenplot/expression.py
+++ src/frankenplot/expression.py
@@ -1,15 +1,99 @@
 #
-# frankenplot.expression: expression parser
+# frankenplot.expression: expression handling
 #
 
 import re
+
+from frankenplot import defaults
+
+# ============================================================================
+
+class Expression(object):
+    def __str__(self):
+        raise NotImplementedError
+
+    def on_plot(self, figure, app):
+        pass
+
+class FluorExpression(Expression):
+    def __init__(self, corrected=None, normalize=None, z_name=None):
+        if corrected is None:
+            self.corrected = defaults.fluor_mode.corrected
+        else:
+            self.corrected = corrected
+
+        if normalize is None:
+            self.normalize = defaults.fluor_mode.normalize
+        else:
+            self.normalize = normalize
+
+        if z_name is None:
+            self.z_name = defaults.z_name
+        else:
+            self.z_name = z_name
+
+    def __str__(self):
+        if self.normalize:
+            return self._normalize_expr(self.expr, self.z_name)
+        else:
+            return self.expr
+
+    def _normalize_expr(self, expression, z_name):
+        return "(%s)/$%s" % (expression, z_name)
+
+class ROIExpression(FluorExpression):
+    def __init__(self, roi, *args, **kwargs):
+        self.roi = roi
+
+        FluorExpression.__init__(self, *args, **kwargs)
+
+    def group_name(self):
+        if self.corrected:
+            stem = "corr_roi"
+        else:
+            stem = "roi"
+
+        return "%%%s_%d" % (stem, self.roi)
+
+    def __str__(self):
+        self.expr = self.group_name()
+        return FluorExpression.__str__(self)
+
+    def on_plot(self, figure, app):
+        app.plot_cp.roi_selector.SetValue(str(self.roi))
+
+class ChannelExpression(FluorExpression):
+    def __init__(self, roi, channel, *args, **kwargs):
+        self.roi = roi
+        self.channel = channel
+
+        FluorExpression.__init__(self, **kwargs)
+
+    def column_name(self):
+        if self.corrected:
+            stem = "corr_roi"
+        else:
+            stem = "roi"
+
+        return "%s%d_%d" % (stem, self.channel, self.roi)
+
+    def __str__(self):
+        self.expr = "$%s" % self.column_name()
+        return FluorExpression.__str__(self)
+
+class TransExpression(Expression):
+    pass
+
+class SampleExpression(TransExpression):
+    pass
+
+class ArbitraryExpression(Expression):
+    pass
 
 # ============================================================================
 
 # groups are signified by a percent sign (e.g., '%roi1')
 GROUP_RE = re.compile(r'%[a-zA-Z_][a-zA-Z0-9_]*')
-
-# ============================================================================
 
 def expand_groups(expression, groups):
     """Expand any group names into sums over their constituent columns
@@ -24,13 +108,3 @@ def expand_groups(expression, groups):
         return "(%s)" % "+".join(["$" + col for col in groups[group]])
 
     return GROUP_RE.sub(group_repl, expression)
-
-def normalize(expression, z_name):
-    return "(%s)/$%s" % (expression, z_name)
-
-# FIXME: may be broken
-def _denormalize(expression, z_name):
-    # FIXME: also remove enclosing parens
-    ptn = r'/\$%s\Z' % (z_name)
-    norm_re = re.compile(ptn)
-    return re.sub(ptn, "\1", expression)
