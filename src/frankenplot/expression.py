d55d4f1c8c0736a5312cfd74b867071be2436899
Jason Petsod <jason@petsod.org>
2010-05-04 14:35:47 -0500

    implement ArbitraryExpression

--- src/frankenplot/expression.py
+++ src/frankenplot/expression.py
@@ -94,7 +94,11 @@ class SampleExpression(TransExpression):
     pass
 
 class ArbitraryExpression(Expression):
-    pass
+    def __init__(self, expr):
+        self.expr = expr
+
+    def __str__(self):
+        return expr
 
 # ============================================================================
 
