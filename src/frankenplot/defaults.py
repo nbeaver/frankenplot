751a5a46fc0ed6ee61003b10325115f7b6ee1e3d
Jason Petsod <jason@petsod.org>
2010-05-04 13:09:47 -0500

    add fluorescence mode defaults

--- src/frankenplot/defaults.py
+++ src/frankenplot/defaults.py
@@ -2,7 +2,15 @@
 # frankenplot.defaults: default values for various parameters
 #
 
+class Mode:
+    pass
+
 x_name = "sam_hor"
 y_name = "sam_vert"
 z_name = "Io"
 colormap = "hot"
+title = ""
+
+fluor_mode = Mode()
+fluor_mode.normalize = True
+fluor_mode.corrected = True
