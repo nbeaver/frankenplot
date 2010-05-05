40e3cfa0e8979a5d6cf2f6b3a16b74c6d4bffff4
Jason Petsod <jason@petsod.org>
2010-05-05 11:08:57 -0500

    add Transmission Mode defaults

--- src/frankenplot/defaults.py
+++ src/frankenplot/defaults.py
@@ -5,12 +5,27 @@
 class Mode:
     pass
 
+# global defaults
 x_name = "sam_hor"
 y_name = "sam_vert"
 colormap = "hot"
 title = ""
 
+# Fluorescence Mode defaults
 fluor_mode = Mode()
 fluor_mode.normalize = True
 fluor_mode.corrected = True
 fluor_mode.z_name = "Io"
+
+# Transmission Mode defaults
+trans_mode = Mode()
+
+# Transmission/Sample Mode defaults
+trans_mode.samp_mode = Mode()
+trans_mode.samp_mode.Io = "Io"
+trans_mode.samp_mode.It = "It"
+
+# Transmission/Reference Mode defaults
+trans_mode.ref_mode = Mode()
+trans_mode.ref_mode.Io = "It"
+trans_mode.ref_mode.It = "Iref"
