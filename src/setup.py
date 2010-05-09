bb9c6d10b93f877d1d2ea676adbd8f3a17bccb7e
Jason Petsod <jason@petsod.org>
2010-05-09 14:58:31 -0500

    get setup.py version from frankenplot module

--- src/setup.py
+++ src/setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from frankenplot import __version__
 
 setup(name="frankenplot",
-      # FIXME: get this from frankenplot module
-      version="2.0a1",
+      version=__version__,
       description="Quick 'n dirty plotting of MRCAT XRF mapping data",
       author="Ken McIvor",
       author_email="mcivor@iit.edu",
